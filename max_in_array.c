97ea56c13ed45da97283f095f5a0e5365f3cc93b
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-25 02:53:13 +0100

    prem

---
+++ max_in_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    int i, j, n;
+    scanf("%d", &n);
+    int tab[n], tempt[2];
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d ",&tab[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = i + 1; j < n; j++)
+        {
+            if(tab[i] > tab[j])
+            {
+                tempt[0] = tab[i]; tab[i] = tab[j]; tab[j] = tempt[0];
+            }
+        }
+    }
+
+    printf("Le maximum est : %d", tab[n-1]);
+    return 0;
+}
