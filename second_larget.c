9da5f2ccb832b845537f1a3e805b061bb0f7b560
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-26 01:47:05 +0100

    mod

--- second_larget.c
+++ second_larget.c
@@ -1,40 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void second()
-{
-    int i, j, n;
-    scanf("%d", &n);
-    int tab[n], tempt[2];
-    for(i = 0; i < n; i++)
-    {
-        scanf("%d",&tab[i]);
-    }
-
-    for(i = 0; i < n; i++)
-    {
-        for(j = i + 1; j < n; j++)
-        {
-            if(tab[i] > tab[j])
-            {
-                tempt[0]=tab[i]; 
-                tab[i]=tab[j]; 
-                tab[j]=tempt[0];
-            }
-        }
-    }
-
-    int k=1;
-    while((tab[n-k] == tab[n-k-1]))
-    {
-        k = k + 1;
-
-    }
-    printf("Le deuxième plus grand nombre est : %d", tab[n-k-1]);
-    
-}
 int main()
 {
-    second();
+    // declaration des variables
+    int n, i, j, max, second_max;
+    printf("Entrez le nombre d'elements du tableau: ");
+    scanf("%d", &n);
+    int tab[n];
+    // saisie des elements du tableau
+    printf("Entrez les elements du tableau:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &tab[i]);
+    }
+max=0;
+    // recherche du maximum
+    for (i = 0; i < n; i++) {
+        if (tab[i] > max) {
+            max = tab[i];
+        }
+    }
+    second_max = -1;
+    // recherche du second maximum
+    for (i = 0; i < n; i++) {
+        if (tab[i] > second_max && tab[i] < max) {
+            second_max = tab[i];
+        }
+    }
+    if (second_max == -1) {
+        printf("Il n'y a pas de second maximum.\n");
+    } else {
+        printf("Le deuxième plus grand nombre est : %d\n", second_max);
+    }
     return 0;
 }
