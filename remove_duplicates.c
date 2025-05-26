6f44c452035cb607f0cdadc46283d772770030f5
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-26 10:23:16 +0100

    nv

---
+++ remove_duplicates.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    int i, j, n;
+    scanf("%d", &n);
+    int tab[n], tempt[2];
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d ",&tab[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = i + 1; j < n; j++)
+        {
+            if(tab[i] > tab[j])
+            {
+                tempt[0] = tab[i]; tab[i] = tab[j]; tab[j] = tempt[0];
+            }
+        }
+    }
+    int m = n;
+    for (i = 0; i < m; i++)
+    {
+        for (j = i + 1; j < m; j++)
+        {
+            if (tab[i] == tab[j])
+            {
+                for (int k = j; k < m - 1; k++)
+                {
+                    tab[k] = tab[k + 1];
+                }
+                m--;
+                j--;
+            }
+        }
+    }
+    printf("Après suppression des doublons : ");
+    for(i = 0; i < m; i++)
+    {
+        printf("%d ", tab[i]);
+    }
+
+}
