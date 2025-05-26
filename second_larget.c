0640defd2dcee8fd8e1fd866a2117e4a73c090df
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-26 23:07:31 +0100

    mod

--- second_larget.c
+++ second_larget.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 int main(){
-    int n, i, max, second_max, temp;
-    scanf("%d", &n);
-    if (n < 2) {
-        return 1;
-    }
+    int n, i;
+    scanf("%d",&n);
     int tab[n];
-    for (i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++){
         scanf("%d", &tab[i]);
     }
-    max = tab[n-1];
-    second_max = -2147483648; // Initialiser à la plus petite valeur possible pour un int
-    for (i = 0; i < n; i++) {
-        if (tab[i] > max) {
-            second_max = max;
-            max = tab[i];
-        } else if (tab[i] > second_max && tab[i] < max) {
-            second_max = tab[i];
+    int j;
+    for(i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(tab[i] > tab[j]){
+                int temp = tab[i];
+                tab[i] = tab[j];
+                tab[j] = temp;
+            }
         }
     }
 
-    printf("Le deuxi\xc3\xa8me plus grand nombre est :%d\n", second_max);
-    return 0;   
+    for(i < n; i = 0; i--){
+        if(tab[i] != tab[n-1]){
+            printf("Le deuxi\xC3\xa8me plus grand nombre est : %d\n", tab[i]);
+            break;
+        }
+    }
+
+    return 0;
 
     
 }
