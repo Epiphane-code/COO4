8cc45cf36bbac022754c1c16dffbd94711cbe8c2
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-26 18:49:48 +0100

    mod

--- second_larget.c
+++ second_larget.c
@@ -2,30 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 int main(){
-    int n, i, j, max, second_max;
+    int n, i, max, second_max;
     scanf("%d", &n);
     if (n < 2) {
-        printf("Minimum taille 2.\n");
         return 1;
     }
     int tab[n];
     for (i = 0; i < n; i++) {
         scanf("%d", &tab[i]);
     }
-    max = second_max = -2147483648;
+    max = tab[0];
+
     for (i = 0; i < n; i++) {
         if (tab[i] > max) {
-            second_max = max;
             max = tab[i];
-        } else if (tab[i] > second_max && tab[i] < max) {
+        }
+    }
+    second_max = -1;
+    for (i = 0; i < n; i++) {
+        if (tab[i] > second_max && tab[i] < max) {
             second_max = tab[i];
         }
     }
-    if (second_max == -2147483648) {
-        printf(".\n");
-    } else {
-        printf("Le deuxième plus grand nombre est : %d\n", second_max);
-    }
+
+
+    printf("Le deuxieme plus grand nombre est : %d\n", second_max);
     return 0;   
 
     
