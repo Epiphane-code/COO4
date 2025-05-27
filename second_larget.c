9bf0998938f9f88ae9fee827384e2f1ab230032e
Epiphane Nicoue Omar <omarepiphanefarouk@gmail.com>
2025-05-27 21:56:04 +0100

    Update second_larget.c

--- second_larget.c
+++ second_larget.c
@@ -1,35 +1,21 @@
-#include <stdio.h>int main(){
+#include <stdio.h>
+#include <limites.h>
+int main(){
     int i, j, n;
     scanf("%d",&n);
     int tab[n];
+    int max = INT_MIN;
+    int second = INT_MIN;
     for(int i = 0; i < n; i++){
         scanf("%d", &tab[i]);
-    }
-    for(i = 0; i < n; i++){
-        for(j = i + 1; j < n; j++){
-            if(tab[i] > tab[j]){
-                int temp = tab[i];
-                tab[i] = tab[j];
-                tab[j] = temp;
-            }
+        if (tab[i] > max){
+            second = max;
+            max = tab[i];
+        }
+        if (tab[i] > min && tab[i] < max ){
+            max = tab[i];
         }
     }
-    int second_largest = tab[n - 2];
-    int k = n - 2;
-    if (n < 2) {
-        return 1;
-    }
-    while (k >= 0 && tab[k] == tab[n - 1]) {
-        k--;
-    }
-    if (k < 0) {
-        return 1;
-    }
-    if (k >= 0) {
-        second_largest = tab[k];
-    }
-    printf("Le deuxième plus grand nombre est : %d\n", second_largest);
-    return 0;
+    printf("Le deuxième plus grand nombre est : %d", second);
 
-    
 }
