083a87232fdb7a3482618d9cdd4ce078b1ea0e04
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-27 09:57:25 +0100

    test

--- second_larget.c
+++ second_larget.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 int main(){
-    int n, i;
+    int i, j, n;
     scanf("%d",&n);
     int tab[n];
     for(int i = 0; i < n; i++){
         scanf("%d", &tab[i]);
     }
-    int j;
     for(i = 0; i < n; i++){
-        for(int j = i + 1; j < n; j++){
+        for(j = i + 1; j < n; j++){
             if(tab[i] > tab[j]){
                 int temp = tab[i];
                 tab[i] = tab[j];
@@ -16,23 +15,18 @@ int main(){
             }
         }
     }
-    int second_largest = tab[n-1];
-    if(n < 2){
-        printf("Il n'y a pas de deuxi\xC3\xa8me plus grand nombre.\n");
-        return 0;
-    }
-
+    int m = n;
     for(i = 0; i < n; i++){
-        if(tab[i] != tab[n-1]){
-            second_largest = tab[i];
-            while(tab[i] == tab[i+1]){
-                i++;
+        if(tab[i] == tab[n-1]){
+            m--;
+            for(j = i + 1; j < n; j++){
+                tab[j-1] = tab[j];
             }
-            
         }
     }
 
-    if(tab[i] != tab[n-1])
+    int second_largest = tab[m-1];
+
     printf("Le deuxi\xC3\xa8me plus grand nombre est : %d\n",second_largest);
             
             
