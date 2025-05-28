b28f8cba27c51289cf1f6b5ee353d7594e43cff3
Epiphane Nicoue Omar <omarepiphanefarouk@gmail.com>
2025-05-28 17:07:22 +0100

    Update second_larget.c

--- second_larget.c
+++ second_larget.c
@@ -1,2 +1,23 @@
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+    int i, n;
+    scanf("%d",&n);
+    int m;
+    int max = INT_MIN;
+    int second = INT_MIN;
+    for(int i = 0; i < n; i++){
+        scanf("%d", &tab[i]);
+        if (m > max){
+            second = max;
+            max = m;
+        }
+        else if (m > second && m != max ){
+            second = m;
+        }
+    }
+    printf("Le deuxième plus grand nombre est : %d", second);
+    return 0;
 
-  
+}
