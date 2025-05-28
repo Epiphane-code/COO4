b428afc82f6039cffd1d3d2150546631a7ea7b38
Epiphane Nicoue Omar <omarepiphanefarouk@gmail.com>
2025-05-28 17:46:52 +0100

    Update second_larget.c

--- second_larget.c
+++ second_larget.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <limits.h>
 
-int second_plus_grand(int tab[], int taille) {
+int second_plus_grand(int taille) {
     if (taille < 2) {
         return INT_MIN;  // Indique qu'il n'y a pas de second plus grand
     }
-
+    int tab[n];
     int max = INT_MIN;
     int second = INT_MIN;
 
     for (int i = 0; i < taille; i++) {
-        if (tab[i] > max) {
+        scanf("%d",&tab[i]);
+%       if (tab[i] > max) {
             second = max;
             max = tab[i];
         } else if (tab[i] > second && tab[i] < max) {
@@ -19,4 +20,10 @@ int second_plus_grand(int tab[], int taille) {
     }
 
     return (second == INT_MIN) ? INT_MIN : second;
+}
+int main ()
+{
+    int n;
+    int second = second_plus_grand(n);
+    printf("Le deuxième plus grand nombre est : %d", second );
 }
