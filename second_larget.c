9c8f3067638af167a16aea6037b1e8e71b4206ee
Epiphane Nicoue Omar <omarepiphanefarouk@gmail.com>
2025-05-28 15:31:42 +0100

    Update second_larget.c

--- second_larget.c
+++ second_larget.c
@@ -5,17 +5,20 @@ int main()
     int i, n;
     scanf("%d",&n);
     int tab[n];
-  for(int i = 0; i < n; i++){
-        scanf("%d", &tab[i]);
-        if (tab[i] > tab[0]){
-            tab[0] = tab[I];
-            
-        }
-        else if (tab[i] > second && tab[i] != max ){
-            second = tab[i];
-        }
-    }
-    printf("Le deuxième plus grand nombre est : %d", second);
+scanf("%d",&tab[0]);
+scanf("%d",&tab[1]);
+  for(int i = 2; i < n; i++){
+      scanf("%d",&tab[i]);
+      if (tab[i] > tab[0]){
+          tab[1] = tab[0];
+          tab[0] = tab[i];
+      }
+      else if (tab[i] > tab[1] && tab[i] != tab[0]){
+          tab[1] = tab[i];
+      }
+  }
+    
+printf("Le deuxième plus grand nombre est : %d", tab[1] );
     return 0;
 
 }
